5af95e456300a94200eeb98473b5b2ddd1359a1d
Denis Subbotin <d.savvato@gmail.com>
2019-06-15 00:27:15 +0300

    Renamed Customer.WriteApi

---
+++ src/ProjectG.CustomerService/ProjectG.CustomerService.WriteApi/Commands/CreateCustomerCommand.cs
@@ -0,0 +1,26 @@
+﻿namespace ProjectG.CustomerService.Api.Commands
+{
+    using System.Threading.Tasks;
+
+    using ProjectG.Core;
+    using ProjectG.CustomerService.Api.DTO;
+    using ProjectG.CustomerService.Api.Extensions;
+    using ProjectG.CustomerService.Core.Interfaces;
+    using ProjectG.CustomerService.Core.Models;
+
+    public class CreateCustomerCommand : ICommandHandler<CustomerCreationModel>
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        public CreateCustomerCommand(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public async Task Execute(CustomerCreationModel commandData)
+        {
+            Customer customer = commandData.ToCustomer();
+            await this.customerRepository.Add(customer);
+        }
+    }
+}
---
+++ src/ProjectG.CustomerService/ProjectG.CustomerService.WriteApi/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿namespace ProjectG.CustomerService.Api.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using ProjectG.Core;
+    using ProjectG.CustomerService.Api.DTO;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICommandHandler<CustomerCreationModel> createCustomerCommand;
+
+        public CustomerController(ICommandHandler<CustomerCreationModel> createCustomerCommand)
+        {
+            this.createCustomerCommand = createCustomerCommand;
+        }
+
+        public async Task<IActionResult> Post([FromBody] CustomerCreationModel customerCreationModel)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            await this.createCustomerCommand.Execute(customerCreationModel);
+
+            return this.Accepted();
+        }
+    }
+}
