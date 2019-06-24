833c13ec96842f5e1282f0932ffbe8c81ab58af3
Denis Subbotin <denis.subbotin@akvelon.com>
2019-06-24 18:01:27 +0300

    Implemented queue messages consumption

---
+++ src/ProjectG.OrderService/ProjectG.OrderService.OrderBuilder/Background/QueueConsumer.cs
@@ -0,0 +1,66 @@
+﻿namespace ProjectG.OrderService.OrderBuilder.Background
+{
+    using System;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
+
+    public class QueueConsumer : BackgroundService
+    {
+        private const string QueueName = "order-creator-queue";
+
+        private readonly ConnectionFactory connectionFactory;
+        private readonly ILogger<QueueConsumer> logger;
+
+        public QueueConsumer(
+            IConfiguration configuration, 
+            ILogger<QueueConsumer> logger)
+        {
+            this.logger = logger;
+            this.connectionFactory = new ConnectionFactory
+            {
+                HostName = configuration["RabbitMQ:HostName"],
+                UserName = configuration["RabbitMQ:UserName"],
+                Password = configuration["RabbitMQ:Password"]
+            };
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (IConnection connection = this.connectionFactory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: QueueName,
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false);
+
+                    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, eventArgs) =>
+                    {
+                        byte[] body = eventArgs.Body;
+                        string message = Encoding.UTF8.GetString(body);
+                        this.logger.LogInformation($"RABBITMQ: consumpted {message}");
+                    };
+                    channel.BasicConsume(
+                        queue: QueueName, 
+                        autoAck: true, 
+                        consumer: consumer);
+
+                    while (!stoppingToken.IsCancellationRequested)
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/ProjectG.OrderService/ProjectG.OrderService.OrderBuilder/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectG.OrderService.OrderBuilder
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using ProjectG.OrderService.OrderBuilder.Background;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHostedService<QueueConsumer>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+        }
+    }
+}
